e59b5cd006ce695d1bbc4bbef7f79c6fdc3b1559
Edgar Delgado <delga157@mail.chapman.edu>
2018-02-21 20:14:46 -0800

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  WarApp
+//
+//  Created by Edgar Delgado on 2/12/18.
+//  Copyright © 2018 Edgar Delgado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    var leftScore = 0
+    var rightScore = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func dealTapped(_ sender: Any) {
+        //Randomize two numbers
+        let leftRandomNumber = arc4random_uniform(13) + 2
+        let rightRandomNumber = arc4random_uniform(13) + 2
+        
+        //Change the image views
+        leftImageView.image = UIImage(named: "card\(leftRandomNumber)")
+        rightImageView.image = UIImage(named: "card\(rightRandomNumber)")
+        
+        
+        //Compare the random numbers
+        if leftRandomNumber > rightRandomNumber{
+            //update score
+            leftScore += 1
+            
+            //update label
+            leftScoreLabel.text = String(leftScore)
+        }
+        else if rightRandomNumber > leftRandomNumber{
+            //update score
+            rightScore += 1
+            
+            //update label
+            rightScoreLabel.text = String(rightScore)
+            
+        }
+        else{
+            
+        }
+        
+    }
+    
+
+}
+
